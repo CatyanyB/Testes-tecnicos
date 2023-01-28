b64889d7c104ae319177061442e2e47b0e620948
CatyanyB <111537027+CatyanyB@users.noreply.github.com>
2023-01-27 21:10:36 -0300

    Add files via upload

---
+++ adicionar-produto-carrinho.cy.js
@@ -0,0 +1,43 @@
+/// <reference types="Cypress" />
+
+describe("Validar adição de produtos ao carrinho", () => {
+
+    it("Deve fazer uma pesquisa no filtro", () => {
+        cy.visit('https://www.amazon.com.br/'); 
+
+        cy.get('#twotabsearchtextbox').click();
+        cy.get('#twotabsearchtextbox').type("Mochila de carrinho infantil menina")
+        cy.get('#nav-search-submit-button').click();
+        cy.get('.s-no-outline > .a-size-medium-plus').should('contain', "RESULTADOS")
+    });
+
+    it("Deve selecionar um produto e adiciona-lo ao carrinho", () => {
+        cy.visit('https://www.amazon.com.br/s?k=mochila+de+carrinho+infantil+menina&crid=2PW6SMHF7NTR2&sprefix=%2Caps%2C1279&ref=nb_sb_ss_recent_3_0_recent');
+        cy.get('[data-asin="B0BQ19F2DB"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .a-spacing-small > .s-title-instructions-style > .a-size-mini > .a-link-normal > .a-size-base-plus').click();
+        cy.get('#title').should('contain', "Mochila com Rodinhas 16 Frozen R 10610 - Xeryus 2023")
+        cy.get('#add-to-cart-button').click();
+        cy.get('.a-size-medium-plus').should('contain', "Adicionado ao carrinho")
+    });
+    });
+
+describe("Teste para calcular valor de todos produtos do carrinho", () =>{
+        function soma(param1, param2, param3) {
+        let result = param1 + param2 + param3;
+        return result;
+        }
+   
+       it("Deve conter 3 itens no carrinho", () => {
+       cy.visit('https://www.amazon.com.br/');
+       cy.get('#twotabsearchtextbox').type("caderno frozen");
+       cy.get('#nav-search-submit-button').click();
+       cy.get('.s-no-outline > .a-size-medium-plus').should('contain', "RESULTADOS");
+       cy.get('[data-asin="B08TQ7X6WN"] > .sg-col-inner > .s-widget-container > .s-card-container > .a-spacing-base > .s-product-image-container > .rush-component > .a-link-normal > .a-section > .s-image').click();
+       cy.get('#a-autoid-6 > .a-button-inner > .a-button-input').click();
+       cy.get('#sw-gtc > .a-button-inner > .a-button-text').click();
+       cy.get('h1').should('contain', "Carrinho de compras");
+   });
+   
+       it("Calculo do valor total no carrinho", () => {
+       expect(soma(74.91, 5.99, 11.20)).to.equal(92.10)
+   });
+   });
---
+++ cadastro-novo-usuario.cy.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe("Teste de cadastro de novo usuário", () => {
+
+    it("Deve criar cadastro de novo usuário com sucesso", () => {
+        cy.visit('https://www.amazon.com.br/');
+        cy.get('#nav-link-accountList > .nav-line-2 > .nav-icon').click();
+        cy.get('#createAccountSubmit').click();
+        cy.get(':nth-child(1) > .a-spacing-small').should('contain', "Criar conta");
+        cy.get('#ap_customer_name').type("Um nome bem legal");
+        cy.get('#ap_email').type("um.email@legal.com");
+        cy.get('#ap_password').type("umasenhasegura123");
+        cy.get('#ap_password_check').type("umasenhasegura123");
+        cy.get('#legalTextRow').should('contain', "Ao criar uma conta, você concorda com as Condições de Uso da Amazon. Por favor verifique a Notificação de Privacidade, Notificação de Cookies e a Notificação de Anúncios Baseados em Interesse.")
+})
+})
